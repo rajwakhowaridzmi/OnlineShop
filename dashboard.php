918a5bb01f0f7a93296f051da76181f9f972332c
rajwakhowaridzmi <rajwakhowaridzmi07@gmail.com>
2023-11-17 07:44:18 +0700

    Online Shop

---
+++ dashboard.php
@@ -0,0 +1,52 @@
+<?php 
+    session_start();
+    if($_SESSION['status_login'] != true){
+        echo'<script>window.location="login.php"</script>';
+
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olshop</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!-- header -->
+    <header>
+        <div class="container">
+        <h1><a href="dashboard.php">TokoCina</a></h1>
+        <ul>
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="profil.php">Profil</a></li>
+            <li><a href="data-kategori.php">Data Kategori</a></li>
+            <li><a href="data-produk.php">Data Produk</a></li>
+            <li><a href="keluar.php">Logout</a></li>
+        </ul>
+        </div>
+    </header>
+
+    <!-- content -->
+    <div class="section">
+        <div class="container">
+            <h3>Dashboard</h3>
+            <div class="box">
+                <h4>Selamat Datang <?php echo $_SESSION['a_global']->admin_name ?> di TokoCina </h4>
+            </div>
+        </div>
+    </div>
+
+    <!-- footer -->
+    <footer>
+        <div class="container">
+            <small>Copyrigt &copy; 2023 - TokoCina.</small>
+        </div>
+    </footer>
+</body>
+</html>
